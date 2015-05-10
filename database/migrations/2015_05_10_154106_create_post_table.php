eb8f3abe1b673976512f4f4e26098cdd59e6973b
alvin joseph valdez <vlainlawliet@gmail.com>
2015-05-11 00:11:51 +0800

    initial commit with migrations created

---
+++ database/migrations/2015_05_10_154106_create_post_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('post', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('address');
+			$table->string('description');
+			$table->string('longtitude');
+			$table->string('latitude');
+			$table->integer('type_id');
+			$table->integer('tag_id');
+			$table->string('price');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('post');
+	}
+
+}
